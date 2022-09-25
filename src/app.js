20df58767f77bec67658f15471fd421f23599973
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-09-25 03:09:52 -0300

    Ajustes para deploy Heroku

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import db from './db';
 
 const app = express();
 app.use(cors());
 dotenv.config();
 
-app.listen(process.env.PORTA,()=>{
-    console.log(`App listening on port ${process.env.PORTA}`);
+/* app.post("/teste", (req, res) => {
+	db.collection("teste").insertOne({
+		teste: "string de teste",
+	});
+}); */
+
+app.listen(process.env.PORT,()=>{
+    console.log(`App listening on port ${process.env.PORT}`);
 });
