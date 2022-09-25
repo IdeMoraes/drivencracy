ed8959c53ded461d162822b9e03ad9f77a6a76c3
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-09-25 03:51:56 -0300

    Teste Heroku

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import db from './db';
+import db from './db.js';
 
 const app = express();
 app.use(cors());
 dotenv.config();
 
-/* app.post("/teste", (req, res) => {
-	db.collection("teste").insertOne({
-		teste: "string de teste",
-	});
-}); */
+app.get("/", (req,res)=>{
+    res.send('Olá.')
+})
+
+app.post("/teste", async (req, res) => {
+    try {
+        await db.collection("teste").insertOne({
+            teste: "string de teste",
+        });
+        return res.send("Funcionou")
+    } catch (error) {
+        console.log("Erro no try")
+        return
+    }
+});
 
 app.listen(process.env.PORT,()=>{
     console.log(`App listening on port ${process.env.PORT}`);
