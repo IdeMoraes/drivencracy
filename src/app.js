1a418f92c63fd863467fe16bb162921aed57fa89
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-09-25 20:20:24 -0300

    Novo teste

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import db from './db.js';
+import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(cors());
 dotenv.config();
+
+let db;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+mongoClient.connect().then(()=>{
+    db = mongoClient.db('drivencracy');
+});
 
 app.get("/", (req,res)=>{
     console.log(process.env.MONGO_URI);
@@ -20,11 +26,12 @@ app.post("/teste", async (req, res) => {
         });
          res.send("Funcionou")
     } catch (error) {
-        console.log("Erro no try")
+        console.log(error);
         return
     }
 });
 
 app.listen(process.env.PORT,()=>{
+    console.log(`URI = ${process.env.MONGO_URI}`)
     console.log(`App listening on port ${process.env.PORT}`);
 });
