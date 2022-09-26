683aaf34bd88e86d964cf065d8d708d5e87eadb3
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-09-26 00:58:38 -0300

    Teste POST /poll

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
 const app = express();
 app.use(cors());
@@ -14,12 +16,46 @@ mongoClient.connect().then(()=>{
     console.log(`Conectado ao Banco de Dados ${process.env.MONGO_URI}`)
 });
 
-/* setInterval(function () { db.query('SELECT 1'); }, 5000); */
+const enqueteModelo = {
+	_id: ObjectId("54759eb3c090d83494e2d222"),
+	title: 'Qual a sua linguagem de programação favorita?', 
+	expireAt: "2022-02-28 01:00"
+};
+
+const opcaoModelo = { 
+	_id: ObjectId("54759eb3c090d83494e2d999"),
+	title: "Javascript", 
+	pollId: ObjectId("54759eb3c090d83494e2d222") 
+};
+
+const votoModelo = { 
+	_id: ObjectId("54759eb3c090d83494e2d000"),
+	createdAt: "2022-02-13 01:00", 
+	choiceId: ObjectId("54759eb3c090d83494e2d999"), 
+};
 
 app.get("/", (req,res)=>{
     console.log(process.env.MONGO_URI);
     return res.send('Olá.')
 
+})
+
+app.post("/poll", async (req, res)=>{
+    const {title, expireAt} = req.body;
+    const titleSchema = joi.object({title: joi.string().required()});
+    const validation = titleSchema.validate(title);
+    if(validation.error){
+        return res.sendStatus(422);
+    }
+    if(expireAt===""){
+        expireAt = dayjs().add(30, 'day').format('YYYY-MM-DD HH:mm')
+    }
+    try {
+        const poll = await db.collection("polls").insertOne({title, expireAt});
+        return res.status(201).send(poll);
+    } catch (error) {
+        console.log(error.message);
+    }
 })
 
 app.post("/teste", async (req, res) => {
