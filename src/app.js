d717d6bdc45aaf34771b1eaee1e288e6ef6b26d0
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-09-26 02:04:53 -0300

    teste

--- src/app.js
+++ src/app.js
@@ -16,12 +16,6 @@ mongoClient.connect().then(()=>{
     console.log(`Conectado ao Banco de Dados ${process.env.MONGO_URI}`)
 }).catch((error)=>console.log(error.message));
 
-/* const enqueteModelo = {
-	_id: ObjectId("54759eb3c090d83494e2d222"),
-	title: 'Qual a sua linguagem de programação favorita?', 
-	expireAt: "2022-02-28 01:00"
-}; */
-
 /* const opcaoModelo = { 
 	_id: ObjectId("54759eb3c090d83494e2d999"),
 	title: "Javascript", 
